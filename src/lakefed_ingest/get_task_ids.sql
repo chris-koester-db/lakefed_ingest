5b454e8dcf477d05257ead85e47ba1b4f3a1e44a
chris-koester-db <chris-koester-db>
2025-06-29 01:38:19 +0000

    Decouple and update jobs

--- src/lakefed_ingest/get_task_ids.sql
+++ src/lakefed_ingest/get_task_ids.sql
@@ -1,5 +1,7 @@
 -- Get task ids for specified job and task collection
 -- Workflow dynamic value references for SQL output are limited to 1,000 rows and 48 KB
-select id from identifier(:ctrl_catalog || '.' || :ctrl_schema || '.' || :ctrl_table)
+-- Convert array to comma separated string for use with workflow dynamic value references
+select id
+from identifier(:ctrl_catalog || '.' || :ctrl_schema || '.' || :ctrl_table)
 where task_collection = :task_collection
 and task_enabled is true
